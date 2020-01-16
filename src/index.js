a3e99f2a434bc72265a6c1d9c80f910daa882c54
Marcus Reaiche <marcusreaiche@gmail.com>
2020-01-15 21:15:25 -0300

    Add cors to app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const routes = require("./routes");
 
@@ -14,6 +15,9 @@ mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASS}@cluster0-psh5r.mongo
 
 const app = express();
 // Middlewares
+app.use(cors({
+  origin: "http://localhost:3000"
+}));
 app.use(express.json());
 app.use(routes);
 
